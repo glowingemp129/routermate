e364551935879452d20cd2732ed8caf9699150eb
Taqi Ul Hassan <glowingemp129@glowingsoft.com>
2025-09-30 14:59:40 +0500

    Navigate Component and useNavigation hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import {Home} from './components/Home';
 import {ProductDetail} from './components/ProductDetail';
 import {ProductList} from "./components/ProductList";
 import {Contact} from "./components/Contact";
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
+import {Admin} from "./components/Admin";
 
 function App() {
+    const isAdmin = true;
   return (
     <div className="App">
         <Header />
@@ -17,6 +19,7 @@ function App() {
                     <Route path="products" element={<ProductList/>} />
                     <Route path="products/10001" element={<ProductDetail />} />
                     <Route path="contact" element={<Contact />} />
+                    <Route path="admin" element={ isAdmin ? <Navigate to="admin" /> : <Navigate to="/" />} />
                 </Routes>
             </main>
         <Footer />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,18 @@
+import {useNavigate} from "react-router-dom";
+
 export const Contact = () => {
+
+    const Navigate = useNavigate();
+
+    const handleSubmit = () => {
+        console.log("handleSubmit");
+
+        return Navigate("/");
+    }
     return (
-        <div>Contact</div>
+        <>
+            <div className="component">Contact</div>
+            <button onClick={handleSubmit}>Back To Home</button>
+        </>
     )
 }
